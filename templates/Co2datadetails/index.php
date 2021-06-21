e5450208a934d0da21588e63b8089c98763c5750
Myo Naing Winn <myonaingwin.gic@gmail.com>
2021-06-21 15:26:12 +0630

    Dynamic Graph OK B-)

--- templates/Co2datadetails/index.php
+++ templates/Co2datadetails/index.php
@@ -1,89 +1,89 @@
 <?php
-    include("fusioncharts.php");
+include("fusioncharts.php");
 
-    // encode json
-    $jsonTemp = json_encode($temp);
-    $jsonHum = json_encode($hum);
-    $jsonCo2 = json_encode($co2);
-    $jsonNoise = json_encode($noise);
+// encode json
+$jsonTemp = json_encode($temp);
+$jsonHum = json_encode($hum);
+$jsonCo2 = json_encode($co2);
+$jsonNoise = json_encode($noise);
 
-    // schema for fusionchart
-    $schemaTemp = file_get_contents('webroot\json\schemaTemp.json');
-    $schemaHum = file_get_contents('webroot\json\schemaHum.json');
-    $schemaCo2 = file_get_contents('webroot\json\schemaCo2.json');
-    $schemaNoise = file_get_contents('webroot\json\schemaNoise.json');
+// schema for fusionchart
+$schemaTemp = file_get_contents('webroot\json\schemaTemp.json');
+$schemaHum = file_get_contents('webroot\json\schemaHum.json');
+$schemaCo2 = file_get_contents('webroot\json\schemaCo2.json');
+$schemaNoise = file_get_contents('webroot\json\schemaNoise.json');
 
-    // fusionTable for schema and json data
-    $tempFusionTable = new FusionTable($schemaTemp, $jsonTemp);
-    $humFusionTable = new FusionTable($schemaHum, $jsonHum);
-    $co2FusionTable = new FusionTable($schemaCo2, $jsonCo2);
-    $noiseFusionTable = new FusionTable($schemaNoise, $jsonNoise);
+// fusionTable for schema and json data
+$tempFusionTable = new FusionTable($schemaTemp, $jsonTemp);
+$humFusionTable = new FusionTable($schemaHum, $jsonHum);
+$co2FusionTable = new FusionTable($schemaCo2, $jsonCo2);
+$noiseFusionTable = new FusionTable($schemaNoise, $jsonNoise);
 
-    // time series graph
-    $temptimeSeries = new TimeSeries($tempFusionTable);
-    $humtimeSeries = new TimeSeries($humFusionTable);
-    $co2timeSeries = new TimeSeries($co2FusionTable);
-    $noisetimeSeries = new TimeSeries($noiseFusionTable);
+// time series graph
+$temptimeSeries = new TimeSeries($tempFusionTable);
+$humtimeSeries = new TimeSeries($humFusionTable);
+$co2timeSeries = new TimeSeries($co2FusionTable);
+$noisetimeSeries = new TimeSeries($noiseFusionTable);
 
-    $temptimeSeries->AddAttribute('chart', '{"exportenabled":true}');
-    $temptimeSeries->AddAttribute('navigator', '{"enabled":0}');
-    $temptimeSeries->AddAttribute('legend', '{"enabled":"0"}');
+$temptimeSeries->AddAttribute('chart', '{"exportenabled":true}');
+$temptimeSeries->AddAttribute('navigator', '{"enabled":0}');
+$temptimeSeries->AddAttribute('legend', '{"enabled":"0"}');
 
-    $humtimeSeries->AddAttribute('chart', '{"exportenabled":true}');
-    $humtimeSeries->AddAttribute('navigator', '{"enabled":0}');
-    $humtimeSeries->AddAttribute('legend', '{"enabled":"0"}');
+$humtimeSeries->AddAttribute('chart', '{"exportenabled":true}');
+$humtimeSeries->AddAttribute('navigator', '{"enabled":0}');
+$humtimeSeries->AddAttribute('legend', '{"enabled":"0"}');
 
-    $co2timeSeries->AddAttribute('chart', '{"exportenabled":true}');
-    $co2timeSeries->AddAttribute('navigator', '{"enabled":0}');
-    $co2timeSeries->AddAttribute('legend', '{"enabled":"0"}');
+$co2timeSeries->AddAttribute('chart', '{"exportenabled":true}');
+$co2timeSeries->AddAttribute('navigator', '{"enabled":0}');
+$co2timeSeries->AddAttribute('legend', '{"enabled":"0"}');
 
-    $noisetimeSeries->AddAttribute('chart', '{"exportenabled":true}');
-    $noisetimeSeries->AddAttribute('navigator', '{"enabled":0}');
-    $noisetimeSeries->AddAttribute('legend', '{"enabled":"0"}');
+$noisetimeSeries->AddAttribute('chart', '{"exportenabled":true}');
+$noisetimeSeries->AddAttribute('navigator', '{"enabled":0}');
+$noisetimeSeries->AddAttribute('legend', '{"enabled":"0"}');
 
-    // chart object
-    $ChartTemp = new FusionCharts(
-        "timeseries",
-        "MyFirstChart" ,
-        "100%",
-        "450",
-        "temperature",
-        "json",
-        $temptimeSeries
-    );
-    $ChartHum = new FusionCharts(
-        "timeseries",
-        "MyFirstChart2" ,
-        "100%",
-        "450",
-        "humidity",
-        "json",
-        $humtimeSeries
-    );
-    $ChartCo2 = new FusionCharts(
-        "timeseries",
-        "MyFirstChart3" ,
-        "100%",
-        "450",
-        "co2",
-        "json",
-        $co2timeSeries
-    );
-    $ChartNoise = new FusionCharts(
-        "timeseries",
-        "MyFirstChart4" ,
-        "100%",
-        "450",
-        "noise",
-        "json",
-        $noisetimeSeries
-    );
+// chart object
+$ChartTemp = new FusionCharts(
+    "timeseries",
+    "MyFirstChart",
+    "100%",
+    "450",
+    "temperature",
+    "json",
+    $temptimeSeries
+);
+$ChartHum = new FusionCharts(
+    "timeseries",
+    "MyFirstChart2",
+    "100%",
+    "450",
+    "humidity",
+    "json",
+    $humtimeSeries
+);
+$ChartCo2 = new FusionCharts(
+    "timeseries",
+    "MyFirstChart3",
+    "100%",
+    "450",
+    "co2",
+    "json",
+    $co2timeSeries
+);
+$ChartNoise = new FusionCharts(
+    "timeseries",
+    "MyFirstChart4",
+    "100%",
+    "450",
+    "noise",
+    "json",
+    $noisetimeSeries
+);
 
-    // Render the chart
-    $ChartTemp->render();
-    $ChartHum->render();
-    $ChartCo2->render();
-    $ChartNoise->render();
+// Render the chart
+$ChartTemp->render();
+$ChartHum->render();
+$ChartCo2->render();
+$ChartNoise->render();
 ?>
 
 <h2>Dashboard</h2>
@@ -111,53 +111,11 @@
     </section>
 </div>
 <hr id="fhr" class="my-5">
-<h4>Device 001</h4>
-<div class="container-fluid">
-    <div class="row">
-        <div class="col">
-            <div class="card">
-                <div class="card-body">
-                    <h5 class="card-title">Temperature</h5>
-                    <div id="temperature">Chart will render here!</div>
-                    <button type="button" class="btn btn-primary">Button</button>
-                    <button type="button"  class="btn realtime-btn btn-primary-grad btn-sm" id="update-data">Update Data</button>
-                </div>
-            </div>
-        </div>
-        <div class="col">
-            <div class="card">
-                <div class="card-body">
-                    <h5 class="card-title">Humidity</h5>
-                    <div id="humidity">Chart will render here!</div>
-                    <button type="button" class="btn btn-primary">Button</button>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col">
-            <div class="card">
-                <div class="card-body">
-                    <h5 class="card-title">CO2</h5>
-                    <div id="co2">Chart will render here!</div>
-                    <button type="button" class="btn btn-primary">Button</button>
-                </div>
-            </div>
-        </div>
-        <div class="col">
-            <div class="card">
-                <div class="card-body">
-                    <h5 class="card-title">Noise</h5>
-                    <div id="noise">Chart will render here!</div>
-                    <button type="button" class="btn btn-primary">Button</button>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-<hr class="my-4">
+<div id="devicesList"></div>
 
 <script>
+    var titles = ['Temperature', 'Humidity', 'CO2', 'Noise'];
+
     var devices = <?php echo json_encode($devices); ?>;
     // console.log(devices);
 
@@ -182,6 +140,35 @@
         });
 
         return newColumn;
+    }
+
+    // generate device list with graphs
+    $.each(devices, function(index, device) {
+        $('#devicesList').append($('<h4>' + device.device + '</h4>'));
+        $('#devicesList').append($('<div id="container-' + index + '" class="container-fluid"></div>'));
+        // add row
+        for (i = 1; i <= 2; i++) {
+            $('#container-' + index).append($('<div id="row-' + index + '-' + i + '" class="row"></div>'));
+
+            // add column
+            for (j = 0; j < 2; j++) {
+                $('#row-' + index + '-' + i).append($('<div id="row-' + index + '-col-' + i + '-' + j + '" class="col"></div>'));
+                addCard('row-' + index + '-col-' + i + '-' + j, i, j);
+            }
+        }
+        $('#container-' + index).after('<hr class="my-4">');
+    });
+
+    // card details
+    function addCard(id, col, card) {
+        var index = card;
+
+        if (col == 2 && card == 0)
+            index = 2;
+        else if (col == 2 && card == 1)
+            index = 3;
+
+        $('#' + id).append($('<div class="card"><div class="card-body"><h5 class="card-title">' + titles[index] + '</h5><div id="' + id + '-graph"></div><button type="button" class="btn realtime-btn btn-primary btn-sm" id="update-data">Update Data</button></div></div>'));
     }
 </script>
 
@@ -226,17 +213,17 @@
 </style>
 
 <script>
-document.getElementById('update-data').addEventListener('click', function() {
-    $.ajax({
-			url: "<?= $this->Url->build(['controller' => 'Co2datadetails', 'action' => '']) ?>",
-			type: "POST",
-			data: {
-				image: imageURL
-			},
-			dataType: "html",
-			headers: {
-				'X-CSRF-Token': $('meta[name="csrfToken"]').attr('content')
-			}
-		});
-});
+    document.getElementById('update-data').addEventListener('click', function() {
+        $.ajax({
+            url: "<?= $this->Url->build(['controller' => 'Co2datadetails', 'action' => '']) ?>",
+            type: "POST",
+            data: {
+                image: imageURL
+            },
+            dataType: "html",
+            headers: {
+                'X-CSRF-Token': $('meta[name="csrfToken"]').attr('content')
+            }
+        });
+    });
 </script>
